008e1dfeb18fe7710ac9c0ad6f8e96b047911d28
Florentino Ngoc <vitmarkha@icloud.com>
2021-03-21 16:16:52 +0300

    fix

--- minishell/main.c
+++ minishell/main.c
@@ -14,13 +14,10 @@ int main(int argc, char **argv, char **env)
 	write(1, "Welcome to Minishell Fngoc and Drarlean!\n", 41);
 //	parser();
 
-	params = malloc(sizeof(g_list));
+	params = malloc(sizeof (g_list));
 	params->env = env_copy(env);
 
 //	export();
-	printf("%s\n", env_var_full_param(params->env, "PWD"));
-	cd("");
-	printf("%s\n", env_var_full_param(params->env, "PWD"));
 
-	return 0;
+	return (0);
 }

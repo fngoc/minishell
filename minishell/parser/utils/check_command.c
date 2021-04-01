22b5bc701bf04dfcf84b756a97dfce30aa128061
Florentino Ngoc <vitmarkha@icloud.com>
2021-04-01 13:11:38 +0300

    the first part of the forging is done

--- minishell/parser/utils/check_command.c
+++ minishell/parser/utils/check_command.c
@@ -66,23 +66,23 @@
 ** и запись команд в двумерный массив.
 */
 
-char *handling_quotation_marks(char *name, t_parser *p)
-{
-	int i;
-	char *new_name;
+// char *handling_quotation_marks(char *name, t_parser *p)
+// {
+// 	int i;
+// 	char *new_name;
 
-	i = -1;
-	new_name = NULL;
-	while (*name != '\0')
-	{
-		if (*name != '\'' && *name != '\"' && *name != ' ')
-			new_name = ft_strjoin_char_free(new_name, *name);
-		++name;
-	}
-	p->map_comand[++i] = ft_strdup(new_name);
-	free(new_name);
-	return (new_name);
-}
+// 	i = -1;
+// 	new_name = NULL;
+// 	while (*name != '\0')
+// 	{
+// 		if (*name != '\'' && *name != '\"' && *name != ' ')
+// 			new_name = ft_strjoin_char_free(new_name, *name);
+// 		++name;
+// 	}
+// 	p->map_comand[++i] = ft_strdup(new_name);
+// 	free(new_name);
+// 	return (new_name);
+// }
 
 /*
 ** check_command: проверка строки на команды.
@@ -91,31 +91,58 @@ char *handling_quotation_marks(char *name, t_parser *p)
 void	check_command(char *line, t_parser *p)
 {
 	char *name;
+	int i;
+	char *previous_char;
 
 	p->map_comand = ft_calloc(500, sizeof(char *));
+	i = -1;
+	previous_char = NULL;
 	name = NULL;
 	while (*line != ';' && *line != '\0')
-	{
+	{	
 		if (ft_istab(*line))
+		{
+			previous_char = line;
 			++line;
-		else if (*line == '\'')
-		{
-			name = ft_strjoin_char_free(name, *line++);
-			while (*line != '\'' && *line != '\0')
-				name = ft_strjoin_char_free(name, *line++);
-			name = ft_strjoin_char_free(name, *line++);
-		}
-		else if (*line == '\"')
-		{
-			name = ft_strjoin_char_free(name, *line++);
-			while (*line != '\"' && *line != '\0')
-				name = ft_strjoin_char_free(name, *line++);
-			name = ft_strjoin_char_free(name, *line++);
 		}
 		else
-			name = ft_strjoin_char_free(name, *line++);
+		{
+			if (*line == '\"' || *line == '\'')
+			{
+				if (previous_char != NULL && *previous_char == ' ')
+					name = NULL;
+				++line;
+				while ((*line != '\"' && *line != '\'') && *line != '\0')
+				{
+					name = ft_strjoin_char_free(name, *line++);
+				}
+				++line;
+				if (ft_istab(*line) || *line == '\0')
+				{
+					p->map_comand[++i] = ft_strdup(name);
+					free(name);
+				}
+				previous_char = NULL;
+			}
+			else
+			{
+				if (previous_char != NULL && *previous_char == ' ')
+					name = NULL;
+				while (!ft_istab(*line) && *line != '\0')
+				{
+					if (*line == '\"' || *line == '\'')
+						break ;
+					name = ft_strjoin_char_free(name, *line++);
+				}
+				if (*line != '\"' && *line != '\'')
+				{
+					p->map_comand[++i] = ft_strdup(name);
+					free(name);
+				}
+				previous_char = NULL;
+			}
+		}
 	}
-	name = handling_quotation_marks(name, p);
 	send_command_execute(p->map_comand);
 	free_map(p->map_comand);
 	if (ft_strlen(line) > 1)

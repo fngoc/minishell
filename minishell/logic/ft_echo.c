14351af21f3b965a8a1f8f74b6aa8dcab8ee1b89
Florentino Ngoc <vitmarkha@icloud.com>
2021-04-04 13:48:40 +0300

    echo done 5

--- minishell/logic/ft_echo.c
+++ minishell/logic/ft_echo.c
@@ -2,63 +2,44 @@
 #include "../parser/parser.h"
 #include "../libft/libft.h"
 
-static	char	*delet_first(char *str)
-{
-	char *new_char;
-
-	new_char = ++str;
-	return (new_char);
-}
-
 void 	ft_echo(char **map, int n_flag, int flag_param, int flag_quotation_mark)
 {
+	(void)flag_quotation_mark;
 	t_list *tmp;
+	char *str_print;
 	int i;
 	int j;
-	char **map_split;
 
 	tmp = params->env;
-	char *str_print;
-	str_print = NULL;
-	j = -1;
+	str_print = ft_calloc(2, sizeof(char));
 	if (n_flag == 0)
 		i = 0;
 	else
 		i = 1;
 	while (map[++i] != NULL)
 	{
-		if (n_flag == 0)
-		{
-			if (flag_param == 0)
-			{
-				ft_putstr_fd(map[i], 1);
-				ft_putchar('\n');
-			}
-			else
-			{
-				map_split = ft_split(map[i], ' ');
-				while (map_split[++j] != NULL)
-				{
-					if ((str_print = get_var_param(tmp, delet_first(map_split[j]))))
-						ft_putstr_fd(str_print, 1);
-					else if (flag_quotation_mark || map_split[j] != NULL)
-						ft_putstr_fd(map_split[j], 1);
-					ft_putchar('\n');
-				}
-				free_map(map_split);
-			}
-		}
+		if (flag_param == 0)
+			ft_putstr_fd(map[i], 1);
 		else
 		{
-			if (flag_param == 0)
-				ft_putstr_fd(map[i], 1);
-			else
+			j = -1;
+			while (map[i][++j] != '\0')
 			{
-				if ((str_print = get_var_param(tmp, delet_first(map[i]))))
-					ft_putstr_fd(str_print, 1);
-				else if (flag_quotation_mark)
-					ft_putstr_fd(map[i], 1);
+				if (map[i][j] != '$')
+					ft_putchar(map[i][j]);
+				else if (map[i][j] == '$')
+				{
+					++j;
+					while (!ft_istab(map[i][j]) && map[i][j] != '\0')
+						str_print = ft_strjoin_char_free(str_print, map[i][j++]);
+					if ((str_print = get_var_param(tmp, str_print)))
+						ft_putstr_fd(str_print, 1);
+					str_print = NULL;
+				}
 			}
 		}
+		ft_putchar(' ');
 	}
+	if (n_flag == 0)
+		ft_putchar_fd('\n', 1);
 }

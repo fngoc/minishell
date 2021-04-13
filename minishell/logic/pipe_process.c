59ad5c7057a51b4471b03373b2602f914de94c6a
drarlean <azminfantik@gmail.com>
2021-04-13 13:01:07 +0300

    push

--- minishell/logic/pipe_process.c
+++ minishell/logic/pipe_process.c
@@ -40,9 +40,7 @@ void	pipe_process(char **argv, t_parser *p, t_file *file)
 	if (pid == 0)
 	{
 		dup2(file->fd_stdout, STDOUT_FILENO);
-		if (p->flag_red == 0) {
-			send_command_execute(argv, p);
-		}
+		send_command_execute(argv, p);
 		close(file->fd_stdin);
 		close(file->fd_stdout);
 		close(STDOUT_FILENO);

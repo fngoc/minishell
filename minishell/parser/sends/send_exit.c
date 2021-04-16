fd8535a07904e5df01f3476d644c9e39d03df6b8
Florentino Ngoc <vitmarkha@icloud.com>
2021-04-16 16:50:31 +0300

    fix exit -1

--- minishell/parser/sends/send_exit.c
+++ minishell/parser/sends/send_exit.c
@@ -4,9 +4,11 @@
 ** send_exit_norm: функция для нормы.
 */
 
-static	void	send_exit_norm(int flag, char *str)
+static	void	send_exit_norm(int flag, int flag_minus, char *str)
 {
-	if (flag == 0)
+	if (flag == 0 && flag_minus == 1)
+		error("exit", 255);
+	else if (flag == 0)
 		error("exit", ft_atoi(str));
 	else
 	{
@@ -25,9 +27,11 @@ void			send_exit(char **map)
 {
 	int flag;
 	int i;
+	int flag_minus;
 
 	flag = 0;
 	i = -1;
+	flag_minus = 0;
 	if (map[1] != NULL && map[2] != NULL)
 	{
 		write(2, "\033[0;35m(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧  \033[0m", 41);
@@ -38,13 +42,18 @@ void			send_exit(char **map)
 	{
 		while (map[1][++i] != '\0')
 		{
+			if (map[1][0] == '-' && flag_minus == 0)
+			{
+				flag_minus = 1;
+				++i;
+			}
 			if (!ft_isdigit(map[1][i]))
 			{
 				flag = 1;
 				break ;
 			}
 		}
-		send_exit_norm(flag, map[1]);
+		send_exit_norm(flag, flag_minus, map[1]);
 	}
 	else
 		error("exit", 0);
